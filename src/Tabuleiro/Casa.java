f3f44071ef19c2f910412b0a6bab4624397bef40
liviaaloise <livia.aloise@gmail.com>
2018-06-06 15:27:47 -0300

    Add files via upload

--- src/Tabuleiro/Casa.java
+++ src/Tabuleiro/Casa.java
@@ -5,10 +5,12 @@ import Pecas.*;
 public class Casa {
 	public char cor;
 	public PECA peca;
+	public boolean mov;
 	
 	public Casa (char Cor, PECA Peca) {
 		cor = Cor;
 		peca = Peca;
+		mov=false;
 	}
 	
 	public boolean vazia() {
