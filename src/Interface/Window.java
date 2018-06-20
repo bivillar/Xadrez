fffeb1b876944af0b3b57282dacd9d903b02680d
liviaaloise <livia.aloise@gmail.com>
2018-06-20 13:50:31 -0300

    funcionando novo jogo

--- src/Interface/Window.java
+++ src/Interface/Window.java
@@ -3,7 +3,10 @@ package Interface;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import Principal.Controlador;
 import Tabuleiro.*;
@@ -14,27 +17,42 @@ public class Window extends JFrame implements ActionListener{
     public final int LARG_DEFAULT=560;
     public final int ALT_DEFAULT=582;
     public DesenhoTabuleiro tab;
+    private JPanel panel;
+    private JButton novo,carregar;
+    private JLabel l;
+    private Image img;
     
     public Window(Casa t[][]) {
         
-        tab = new DesenhoTabuleiro(t);
+    	tab = new DesenhoTabuleiro(t);
         
-//        JPanel panel = new JPanel();
+        panel = new JPanel();
+        try {
+        	img = ImageIO.read(new File("Imagens/chess.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+			System.exit(1);
+		}
+        l=new JLabel("imagem");
+        l.setIcon(new ImageIcon(img));
+        panel.add(l);
         
-//        novo = new JButton("Novo Jogo");
-//        novo.setVerticalTextPosition(AbstractButton.CENTER);
-//        novo.setHorizontalTextPosition(AbstractButton.LEADING);
-//        novo.setBounds(100, 100, 100, 100);
-//        novo.setActionCommand("novo");
-//        
-//        carregar = new JButton("Carregar Jogo");
-//        carregar.setVerticalTextPosition(AbstractButton.CENTER);
-//        carregar.setHorizontalTextPosition(AbstractButton.CENTER);
-//        carregar.setBounds(100, 100, 100, 100);
-//        carregar.setActionCommand("carregar");
-//        
-//        novo.addActionListener(this);
-//        carregar.addActionListener(this);
+        
+        novo = new JButton("Novo Jogo");
+        novo.setVerticalTextPosition(AbstractButton.CENTER);
+        novo.setHorizontalTextPosition(AbstractButton.LEADING);
+        novo.setBounds(100, 100, 100, 100);
+        novo.setActionCommand("novo");
+        
+        carregar = new JButton("Carregar Jogo");
+        carregar.setVerticalTextPosition(AbstractButton.CENTER);
+        carregar.setHorizontalTextPosition(AbstractButton.CENTER);
+        carregar.setBounds(100, 100, 100, 100);
+        carregar.setActionCommand("carregar");
+        
+        novo.addActionListener(this);
+        carregar.addActionListener(this);
         
         Toolkit tk=Toolkit.getDefaultToolkit();
         Dimension screenSize=tk.getScreenSize();
@@ -46,22 +64,27 @@ public class Window extends JFrame implements ActionListener{
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
         setTitle("Xadrez");
-        getContentPane().add(tab);
+        //getContentPane().add(tab);
         
-//        panel.setPreferredSize(new Dimension(100, 100));
-//        panel.add(novo);
-//        panel.add(carregar);
-//        getContentPane().add(panel, BorderLayout.CENTER);
+        
+        panel.setPreferredSize(new Dimension(100, 100));
+        panel.add(novo);
+        panel.add(carregar);
+        getContentPane().add(panel, BorderLayout.CENTER);
 
     }
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-//		if("novo".equals(e.getActionCommand())) {
-//			getContentPane().add(tab);
-//		}else {
-//			//TODO carregar
-//		}	
+		if("novo".equals(e.getActionCommand())) {
+			panel.setVisible(false);
+			getContentPane().add(tab);
+			
+			
+		}else {
+			//TODO carregar
+			
+		}	
 	}
     
     
