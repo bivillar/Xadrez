f87db967d2ad3ff35c8771afe8b004ec1a861780
bivillar <bianca.villar@dbsoft.com.br>
2018-06-24 18:03:48 -0300

    add files via upload

--- src/Principal/Main.java
+++ src/Principal/Main.java
@@ -1,17 +1,13 @@
 package Principal;
 
 import Interface.*;
-import Tabuleiro.*;
 
 public class Main {
 	
 	static Window janelaJogo;
 	
 	public static void main(String[] args) {
-		
-		//t.imprime();
 		janelaJogo = new Window();
-		
 		janelaJogo.setVisible(true);
 		janelaJogo.addMouseListener(new Controlador());
 	}
