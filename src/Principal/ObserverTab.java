9abb84da4be522964c017af01d34c470f2adba61
liviaaloise <livia.aloise@gmail.com>
2018-06-24 21:02:46 -0300

    Add files via upload

--- src/Principal/ObserverTab.java
+++ src/Principal/ObserverTab.java
@@ -3,16 +3,14 @@ package Principal;
 import java.util.Observable;
 import java.util.Observer;
 
-public class ObserverTab implements Observer {
+public class ObserverTab implements Observer{
 
-	public void notify (ObservadoTab o){
-		Main.janelaJogo.tab.repaint();
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		Main.janelaJogo.repaint();
 	}
 	
-	@Override
-	public void update(Observable arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
+	
 }
